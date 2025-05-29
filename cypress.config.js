0b5c7e00787941a306817ebae86767202bb03ae0
Adrian Ortiz (Qase Demo) <adrianeortizdemo@gmail.com>
2025-05-29 08:34:30 -0700

    updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,13 @@
-import cypress from 'cypress';
+const { defineConfig } = require('cypress');
 
-module.exports = cypress.defineConfig({
+module.exports = defineConfig({
   reporter: 'cypress-multi-reporters',
   reporterOptions: {
     reporterEnabled: 'cypress-qase-reporter',
     cypressQaseReporterReporterOptions: {
+      mode: "testops",
       debug: true,
+      environment: "local",
 
       testops: {
 
@@ -15,22 +17,20 @@ module.exports = cypress.defineConfig({
         run: {
           complete: true,
         },
-        useV2: true,
       },
-
       framework: {
         cypress: {
           screenshotsFolder: 'cypress/screenshots',
-        },
-      },
+        }
+      }
     },
   },
-  video: false,
-  screenshotOnRunFailure: true,
   e2e: {
+    video: false,
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
-      require('cypress-qase-reporter/plugin')(on, config);
-      require('cypress-qase-reporter/metadata')(on);
+      require('cypress-qase-reporter/plugin')(on, config)
+      require('cypress-qase-reporter/metadata')(on)
     },
-  },
+  }
 });
